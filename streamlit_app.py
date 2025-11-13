c3510768d9bef022a7931097042b825d22e7e08f
markhuiqin0831-pixel <markhuiqin0831@gmail.com>
2025-11-13 02:06:40 +0000

    Update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,23 @@
 import streamlit as st
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+import clips
+import logging
+
+logging.basicConfig(level=15,format='%(message)s')
+
+env = clips.Environment()
+router = clips.LoggingRouter()
+env.add_router(router)
+
+name = st.text_input("Enter your name")
+
+env.build('(deftemplate result (slot name))')
+env.assert_string(f'(result(name "{name}"))')
+
+env.run
+
+results=[]
+for fact in env.facts():
+     if fact.template.name =='result':
+          results.append(fact['name'])
+
+st.write(results[0],"output")
